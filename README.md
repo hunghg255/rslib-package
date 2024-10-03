1b1ee17599b3df3d5e49271c350e6e9e52473b44
hunghg255 <giahunghust@gmail.com>
2024-10-03 11:17:17 +0700

    chore: init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Rslib Project
+
+https://github.com/web-infra-dev/rslib
+
+## Setup
+
+Install the dependencies:
+
+```bash
+pnpm install
+```
+
+## Get Started
+
+Build the library:
+
+```bash
+pnpm build
+```
+
+Build the library in watch mode:
+
+```bash
+pnpm build --watch
+```
---
+++ rslib.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from '@rslib/core';
+
+const shared = {
+  dts: {
+    bundle: false,
+  },
+};
+
+export default defineConfig({
+  lib: [
+    {
+      ...shared,
+      format: 'esm',
+      syntax: "es5",
+      output: {
+        distPath: {
+          root: './dist/esm',
+        },
+      },
+    },
+    {
+      ...shared,
+      format: 'cjs',
+      output: {
+        distPath: {
+          root: './dist/cjs',
+        },
+      },
+    },
+  ],
+  output: {
+    target: "node",
+  },
+});
